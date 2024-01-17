c40961d2f4a6b30b52879792ae3456733ce9c094
sheikhHassaan <hassaanbasil@gmail.com>
2024-01-17 20:03:29 +0500

    Implemented InventoryResources.java class.

--- src/main/java/Resources/InventoryResources.java
+++ src/main/java/Resources/InventoryResources.java
@@ -1,11 +1,19 @@
 package Resources;
 
+import Common.ConnectionNotFoundException;
 import Common.SecurityFilter;
+import Domain.Inventory;
 import Services.InventoryServiceImpl;
 import Services.Queries;
 import com.google.gson.Gson;
 import javax.ws.rs.*;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.UUID;
 
 @Path("/inventory")
 public class InventoryResources extends Queries {
@@ -30,9 +38,30 @@ public class InventoryResources extends Queries {
     @GET
     @Path("/{inventory_id}")
     @Produces("text/plain")
-    public Response fetchByID(@PathParam("inventory_id") Integer inventory_id){
+    public Response FetchByID(@PathParam("inventory_id") String inventory_id){
         // note: Have to return "Inventory", no payload.
-        return ;
+        try{
+//            securityFilter.filter(requestContext); @Context ContainerRequestContext requestContext,
+            Inventory inventory = inventoryService.fetchByID(UUID.fromString(inventory_id));
+            if (inventory != null)
+                return Response.ok().entity(gson.toJson(inventory)).build();
+            return Response.status(Response.Status.BAD_REQUEST).entity("Record not found!").build();
+        }
+//        catch (IOException e) {
+//            return Response.status(Response.Status.BAD_REQUEST).entity("Wrong credentials.").build();
+//        }
+        catch (ConnectionNotFoundException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Connection could not be established.").build();
+        }
+        catch (SQLException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("SQL Exception occurred.").build();
+        }
+        catch (ClassNotFoundException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Class not found.").build();
+        }
+        catch (Exception e){
+            return Response.status(Response.Status.BAD_REQUEST).entity("Something went wrong.").build();
+        }
     }
 
     // note: 2. Fetch All, http://localhost:8080/inventory/list (HTTP GET)
@@ -41,9 +70,25 @@ public class InventoryResources extends Queries {
     @Path("/list")
     @Produces("text/plain")
     public Response FetchAll(){
-
         // note: Have to return "ArrayList<Inventory>", no payload.
-        return ;
+        try{
+            ArrayList<Inventory> inventories = inventoryService.fetchAll();
+            if (inventories != null)
+                return Response.ok().entity(gson.toJson(inventories)).build();
+            return Response.status(Response.Status.BAD_REQUEST).entity("No records found!").build();
+        }
+        catch (ConnectionNotFoundException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Connection could not be established.").build();
+        }
+        catch (SQLException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("SQL Exception occurred.").build();
+        }
+        catch (ClassNotFoundException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Class not found.").build();
+        }
+        catch (Exception e){
+            return Response.status(Response.Status.BAD_REQUEST).entity("Something went wrong.").build();
+        }
     }
 
     // note: 3. Fetch All By Category, http://localhost:8080/inventory/listByCategory?category=22 (HTTP GET)
@@ -51,9 +96,26 @@ public class InventoryResources extends Queries {
     @GET
     @Path("/listByCategory")
     @Produces("text/plain")
-    public Response fetchAllByCategory(@QueryParam("category") Integer category_id){
+    public Response FetchAllByCategory(@QueryParam("category") String category_id){
         // note: Have to return "ArrayList<Inventory>", no payload.
-        return ;
+        try{
+            ArrayList<Inventory> inventories = inventoryService.fetchAllByCategory(UUID.fromString(category_id));
+            if (inventories != null)
+                return Response.ok().entity(gson.toJson(inventories)).build();
+            return Response.status(Response.Status.BAD_REQUEST).entity("No records found!").build();
+        }
+        catch (ConnectionNotFoundException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Connection could not be established.").build();
+        }
+        catch (SQLException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("SQL Exception occurred.").build();
+        }
+        catch (ClassNotFoundException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Class not found.").build();
+        }
+        catch (Exception e){
+            return Response.status(Response.Status.BAD_REQUEST).entity("Something went wrong.").build();
+        }
     }
 
     // note: 4. Fetch All By Location, http://localhost:8080/inventory/list?location=7 (HTTP GET)
@@ -61,9 +123,26 @@ public class InventoryResources extends Queries {
     @GET
     @Path("/list")
     @Produces("text/plain")
-    public Response fetchAllByLocation(@QueryParam("location") Integer location_id){
+    public Response FetchAllByLocation(@QueryParam("location") String location_id){
         // note: Have to return "ArrayList<Inventory>", no payload.
-        return ;
+        try{
+            ArrayList<Inventory> inventories = inventoryService.fetchAllByLocation(UUID.fromString(location_id));
+            if (inventories != null)
+                return Response.ok().entity(gson.toJson(inventories)).build();
+            return Response.status(Response.Status.BAD_REQUEST).entity("No records found!").build();
+        }
+        catch (ConnectionNotFoundException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Connection could not be established.").build();
+        }
+        catch (SQLException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("SQL Exception occurred.").build();
+        }
+        catch (ClassNotFoundException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Class not found.").build();
+        }
+        catch (Exception e){
+            return Response.status(Response.Status.BAD_REQUEST).entity("Something went wrong.").build();
+        }
     }
 
     // note: 5. Fetch All By Location And Category, http://localhost:8080/inventory/list?location=7&category=22 (HTTP GET)
@@ -80,9 +159,26 @@ public class InventoryResources extends Queries {
     @GET
     @Path("/list")
     @Produces("text/plain")
-    public Response fetchAllByLocationAndCategory(@QueryParam("location") Integer location_id, @QueryParam("category") Integer category_id){
+    public Response FetchAllByLocationAndCategory(@QueryParam("location") String location_id, @QueryParam("category") String category_id){
         // note: Have to return "ArrayList<Inventory>", no payload.
-        return ;
+        try{
+            ArrayList<Inventory> inventories = inventoryService.fetchAllByLocationAndCategory(UUID.fromString(location_id), UUID.fromString(category_id));
+            if (inventories != null)
+                return Response.ok().entity(gson.toJson(inventories)).build();
+            return Response.status(Response.Status.BAD_REQUEST).entity("No records found!").build();
+        }
+        catch (ConnectionNotFoundException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Connection could not be established.").build();
+        }
+        catch (SQLException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("SQL Exception occurred.").build();
+        }
+        catch (ClassNotFoundException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Class not found.").build();
+        }
+        catch (Exception e){
+            return Response.status(Response.Status.BAD_REQUEST).entity("Something went wrong.").build();
+        }
     }
 
     // note: 6. Add New Inventory Item, http://localhost:8080/inventory/add (HTTP POST)
@@ -99,7 +195,26 @@ public class InventoryResources extends Queries {
     public Response addNew(String inventoryJson){
         // note: Inventory-Json received as payload. Have to parse it and run the add-function on it.
         //  Have to return "Inventory" in response.
-        return ;
+        try{
+            Inventory inventory = gson.fromJson(inventoryJson, Inventory.class);
+            inventory.setId(UUID.randomUUID());
+            Inventory returnInventory = inventoryService.addInventory(inventory);
+            if (returnInventory != null)
+                return Response.ok().entity(gson.toJson(returnInventory)).build();
+            return Response.status(Response.Status.BAD_REQUEST).entity("The record could not be inserted!").build();
+        }
+        catch (ConnectionNotFoundException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Connection could not be established.").build();
+        }
+        catch (SQLException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("SQL Exception occurred.").build();
+        }
+        catch (ClassNotFoundException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Class not found.").build();
+        }
+        catch (Exception e){
+            return Response.status(Response.Status.BAD_REQUEST).entity("Something went wrong.").build();
+        }
     }
 
 
@@ -114,10 +229,29 @@ public class InventoryResources extends Queries {
     @PUT
     @Path("/{inventory_id}")
     @Produces("text/plain")
-    public Response updateByID(@PathParam("inventory_id") Integer inventory_id, String inventoryJson){
+    public Response updateByID(@PathParam("inventory_id") String inventory_id, String inventoryJson){
         // note: Inventory-Json received as payload. Have to parse it and run the update-function on it.
         //  Have to return "Inventory" in response.
-        return ;
+        try{
+            Inventory inventory = gson.fromJson(inventoryJson, Inventory.class);
+            inventory.setId(UUID.fromString(inventory_id));
+            Inventory returnInventory = inventoryService.updateInventory(inventory);
+            if (returnInventory != null)
+                return Response.ok().entity(gson.toJson(returnInventory)).build();
+            return Response.status(Response.Status.BAD_REQUEST).entity("The record could not be updated!").build();
+        }
+        catch (ConnectionNotFoundException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Connection could not be established.").build();
+        }
+        catch (SQLException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("SQL Exception occurred.").build();
+        }
+        catch (ClassNotFoundException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Class not found.").build();
+        }
+        catch (Exception e){
+            return Response.status(Response.Status.BAD_REQUEST).entity("Something went wrong.").build();
+        }
     }
 
     // note: 8. Delete Existing Inventory Item By Id, http://localhost:8080/inventory/{inventory_id}/ (HTTP DELETE)
@@ -129,8 +263,25 @@ public class InventoryResources extends Queries {
     @DELETE
     @Path("/{inventory_id}")
     @Produces("text/plain")
-    public Response delete(@PathParam("inventory_id") Integer inventory_id){
+    public Response Delete(@PathParam("inventory_id") String inventory_id){
         // note: Have to run delete-function on the inventory_id & return "Message" in response. No payload.
-        return ;
+        try{
+            boolean isSuccessful = inventoryService.delete(UUID.fromString(inventory_id));
+            if (isSuccessful)
+                return Response.ok().entity("Inventory deleted successfully.").build();
+            return Response.status(Response.Status.BAD_REQUEST).entity("The record could not be deleted!").build();
+        }
+        catch (ConnectionNotFoundException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Connection could not be established.").build();
+        }
+        catch (SQLException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("SQL Exception occurred.").build();
+        }
+        catch (ClassNotFoundException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Class not found.").build();
+        }
+        catch (Exception e){
+            return Response.status(Response.Status.BAD_REQUEST).entity("Something went wrong.").build();
+        }
     }
 }
